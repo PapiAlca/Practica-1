3c55a0e6b85367ef05b50361adcb9af8b4c2b40d
JuanParejoG <100224095+PapiAlca@users.noreply.github.com>
2022-11-09 20:33:30 +0100

    Add P5-Estructuras de Control/Arbol.java
    Agrego la carpeta P5-Estructuras de Control y la clase objeto Arbol.java

---
+++ Programacion/P5-Estructuras de Control/Arbol.java
@@ -0,0 +1,22 @@
+/* Práctica del Tema 5 "Altura de un árbol general" */
+
+//autor: Juan Parejo García
+
+//Clase Objeto: Arbol.
+public class Arbol {
+    //Variable:
+    char cadena;
+
+    //Constructor:
+    public Arbol(char cadena) {
+        this.cadena = cadena;
+    }
+
+    //Getters y Setters:
+    public char getCadena() {
+        return cadena;
+    }
+    public void setCadena(char cadena) {
+        this.cadena = cadena;
+    }
+}
