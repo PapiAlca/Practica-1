577103801c619b68cc7b20ed2d9dfa0ede08bbdc
JuanParejoG <100224095+PapiAlca@users.noreply.github.com>
2022-11-10 20:14:10 +0100

    Fix main class ProbarArbol

--- Programacion/P5-Estructuras de Control/ProbarArbol.java
+++ Programacion/P5-Estructuras de Control/ProbarArbol.java
@@ -10,18 +10,20 @@ public class ProbarArbol {
         int profundidad = 0;
         Arbol n1 = new Arbol("12300010");
 
-        System.out.println("La cadena de nuestro Árbol es " + n1.cadena + ".");
-        for (int i = 0; i < n1.cadena.length(); i++) {
+        System.out.println("La cadena de nuestro Árbol es " + n1.getCadena() + ".");
+        for (int i = 0; i < n1.getCadena().length(); i++) {
             //leer 1 caracter (charAt());
-            n1.cadena.charAt(i);
-            System.out.println(n1.cadena.charAt(i));
+            n1.getCadena().charAt(i);
+            System.out.println("El nodo " + i +" tiene valor "+n1.getCadena().charAt(i) + ".");
 
             //si el num que se está leyendo es 0 dejar de incrementar profundidad y restarle 1, caso contrario incrementar 1 en profundidad.
-            if (n1.cadena.charAt(i).equals("0")) {
+            if (n1.getCadena().charAt(i) == '0') {
                 profundidad = profundidad - 1;
+                System.out.println("La profundidad es " + profundidad + ".");
             }
             else {
                 profundidad = profundidad + 1;
+                System.out.println("La profundidad es " + profundidad + ".");
             }
         }
 
