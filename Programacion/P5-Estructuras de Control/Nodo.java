1621a43425ecb841d56b4c48dab73511104c9500
JuanParejoG <100224095+PapiAlca@users.noreply.github.com>
2022-11-10 21:09:35 +0100

    Add object class Nodo
    La clase objeto Nodo es una clase nueva que he creado para utilizarla como atributo en la clase objeto Arbol y la clase main ProbarArbol, para mayor comodidad a la hora de crear sentencias y métodos en ambas clases

---
+++ Programacion/P5-Estructuras de Control/Nodo.java
@@ -0,0 +1,57 @@
+/* Práctica del Tema 5 "Altura de un árbol general" */
+
+//autor: Juan Parejo García
+
+//Clase Objeto: Nodo.
+public class Nodo{
+	String valor;
+	int altura;
+	boolean hijo;
+	
+	//Constructor:
+    public Nodo(String valor, int altura, boolean hijo) {
+        this.valor = valor;
+		this.altura = altura;
+		this.hijo = hijo;
+    }
+
+    //Getters y Setters:
+    public String getValor() {
+        return valor;
+    }
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+	
+	public int getAltura() {
+        return altura;
+    }
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+	
+	public boolean getHijo() {
+		return hijo;
+	}
+	public void setBoolean(boolean hijo) {
+		this.hijo = hijo;
+	}
+	
+	//Métodos:
+	public boolean esHijoDe(boolean hijo) {
+		if (this.altura > 1) {
+			hijo = true;
+		}
+		else {
+			hijo = false;
+		}
+		this.hijo = hijo;
+		return hijo;
+	}
+	
+	public void calcularAltura() {
+		if (this.hijo == true && this.valor != "0") {
+			this.altura += 1;
+		}
+	}
+}
