ba0b3e1c39ab0eaad7150af69c8df81c1f673b18
JuanParejoG <100224095+PapiAlca@users.noreply.github.com>
2022-11-10 01:47:51 +0100

    Fix  main class ProbarArbol
    ya están resueltos todos los errores de la clase main ProbarArbol exceptuando un error con el método .equals()

--- Programacion/P5-Estructuras de Control/ProbarArbol.java
+++ Programacion/P5-Estructuras de Control/ProbarArbol.java
@@ -3,28 +3,29 @@
 //autor: Juan Parejo García
 
 //Clase Main: ProbarArbol.
+import java.util.*;
 public class ProbarArbol {
     public static void main(String[] args) {
-		//Variables:
+        //Variables:
         int profundidad = 0;
-        Arbol n1 = new Arbol("");
-		
-		System.out.println("La cadena de nuestro Árbol es " + n1.cadena + ".");
-		for (i = 0, i < n1.cadena.length(), i++) {
-			//leer 1 caracter (charAt());
-			n1.charAt(i)
-			System.out.println(n1.charAt(i));
-			
-			//si el num que se está leyendo es 0 dejar de incrementar profundidad y restarle 1, caso contrario incrementar 1 en profundidad.
-            if(n1.charAt(i)=0) {
-				profundidad = profundidad - 1;
+        Arbol n1 = new Arbol("12300010");
+
+        System.out.println("La cadena de nuestro Árbol es " + n1.cadena + ".");
+        for (int i = 0; i < n1.cadena.length(); i++) {
+            //leer 1 caracter (charAt());
+            n1.cadena.charAt(i);
+            System.out.println(n1.cadena.charAt(i));
+
+            //si el num que se está leyendo es 0 dejar de incrementar profundidad y restarle 1, caso contrario incrementar 1 en profundidad.
+            if (n1.cadena.charAt(i).equals("0")) {
+                profundidad = profundidad - 1;
             }
-			else {
-				profundidad++;
-			}
-		} //cuando se lea la ultima posicion de la cadena se cerrará el bucle
-		
-		//Mostramos cuanta profundidad tiene:
-		System.out.println("Nuestro Árbol tiene " + profundidad + " de profundidad.");
+            else {
+                profundidad = profundidad + 1;
+            }
+        }
+
+        //Mostramos cuanta profundidad tiene nuestro objeto Arbol n1:
+        System.out.println("Nuestro Árbol tiene " + profundidad + " de profundidad.");
     }
 }
