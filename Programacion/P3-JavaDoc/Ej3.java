9ea25f1c9bcd393d1ec1710984cbfed0c3d45cdf
JuanParejoG <100224095+PapiAlca@users.noreply.github.com>
2022-11-24 17:20:33 +0100

    Delete P3-JavaDoc

--- Programacion/P3-JavaDoc/Ej3.java
+++
@@ -1,8 +0,0 @@
-public class Ej3 {
-   public static void main(String[] args) {
-       System.getProperty("user.name");
-           System.out.println(System.getProperty("user.name"));
-       System.getProperty("os.name");
-       System.out.println(System.getProperty("os.name"));
-   }
-}	
