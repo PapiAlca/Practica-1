35424546bd4863384efc70a73191bfe2d9e0bbfa
JuanParejoG <100224095+PapiAlca@users.noreply.github.com>
2023-03-09 21:14:21 +0100

    Add P3-JavaDoc

--- Programacion/P3-JavaDoc/Clase HolaMundo/HolaMundo.java
+++ Programacion/P3-JavaDoc/Clase HolaMundo/HolaMundo.java
@@ -1,8 +1,11 @@
-/**
+/** Esta es la clase HolaMundo y lo que hace es por medio de un println, imprimir por pantalla el texto que tiene. En este caso ¡Hola mundo!.
  * @author Juan Parejo García.
- * Esta es la clase HolaMundo y lo que hace es por medio de un println, imprimir por pantalla el texto que tiene. En este caso ¡Hola mundo!.
+ *
  */
 public class HolaMundo {
+    /** Lo que hace es pillar todos los argumentos del método main
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("¡Hola mundo!");
     }
